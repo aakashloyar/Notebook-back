700734d88f9a09fb6513cec5b6a252357e92a6e9
aakashloyar <aakashloyar@gmail.com>
2024-11-29 18:36:19 +0530

    working on connectivity problem

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 const port = 3000
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 app.use('/api/auth',require('./routes/auth'));
 app.use('/api/notes',require('./routes/notes'));
 
